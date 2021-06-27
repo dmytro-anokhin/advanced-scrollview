d3526b28a222f44bbc2f3722eb698b730e6d9ece
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2021-06-27 09:01:27 +0200

    Introduce Magnification struct

---
+++ Sources/AdvancedScrollView/Magnification.swift
@@ -0,0 +1,18 @@
+//
+//  Magnification.swift
+//  
+//
+//  Created by Dmytro Anokhin on 27/06/2021.
+//
+
+import CoreGraphics
+
+
+public struct Magnification {
+
+    let range: ClosedRange<CGFloat>
+
+    let initialValue: CGFloat
+
+    let isRelative: Bool
+}
